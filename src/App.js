b795ce0f7b75010ccd9c500291c135e99ce4b0c9
alexspel <staflin18@gmail.com>
2021-08-02 16:42:33 +0300

    React lesson 1

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import { Message } from "./Message";
+
+export function App(props) {
+    return (
+        <div className="App">
+            <div>App component text</div>
+            <Message message={props.message} />
+        </div>
+    );
+}
---
+++ src/Message.js
@@ -0,0 +1,6 @@
+import "./Message.css";
+export function Message(props) {
+    return (
+        <h1 className="message">Message component: props.message = {props.message}</h1>
+    );
+}
