f9cc30e129595a2be7c135263a4f7580db49963d
alexspel <staflin18@gmail.com>
2021-08-02 17:00:26 +0300

    React lesson 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
 import { Message } from "./Message";
 
+
 export function App(props) {
     return (
-        <div className="App">
-            <div>App component text</div>
+        <div className="App">            <div>App component text</div>
             <Message message={props.message} />
         </div>
     );
